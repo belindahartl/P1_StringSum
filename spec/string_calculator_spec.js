08e394a1bab504c8307c5ce225bb28eaf20af807
Belinda Hartl <152974825+belindahartl@users.noreply.github.com>
2024-10-12 17:44:49 +0200

    Added Tests and finished Practice 1

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -3,7 +3,13 @@ describe('String Calculator', function() {
 		calculator = new StringCalculator();
 	});
 
-	it('should return 0 for an empty string', function() {
-		expect(calculator.add('')).toEqual(0);
+	it('should return 2 for an empty string', function() {
+		expect(calculator.AddTwoNumbers("1","1")).toEqual("2");
+	});
+	it('should return 1 for an empty string', function() {
+		expect(calculator.AddTwoNumbers("1","asd")).toEqual("1");
+	});
+	it('should return 2 for an empty string', function() {
+		expect(calculator.AddTwoNumbers("asd","asdasd")).toEqual("0");
 	});
 });
--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,5 +1,18 @@
 // StringCalculator.js
-function StringCalculator() {}
-StringCalculator.prototype.add = function(string_numbers) {
-	//
-};
+class StringCalculator{
+	constructor() {}
+		AddTwoNumbers(number1, number2){
+			if(!isNaturalNumber(number1))
+				number1 = 0;
+			if(!isNaturalNumber(number2))
+				number2 = 0;
+
+			return String(parseInt(number1) + parseInt(number2));
+		} 
+}
+function isNaturalNumber(n) {
+    n = n.toString(); // force the value incase it is not
+    var n1 = Math.abs(n),
+        n2 = parseInt(n, 10);
+    return !isNaN(n1) && n2 === n1 && n1.toString() === n;
+}
